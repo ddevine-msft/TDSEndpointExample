ae581021e4cc7cf44e48e3097d1e4c1b77c8316d
Darrin Devine <73505354+ddevine-msft@users.noreply.github.com>
2024-08-30 14:31:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+SqlAuthenticationToken? _sqlAuthenticationToken = null;
+string queryString = "select[Total Row Count] = count(*) from account";
+string retVal = String.Empty;
+
+
+// Build configuration to Retrieve the connection string from appsettings.json 
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+string connectionString = configuration["AppSettings:SQLConnectionString"];
+
+try
+{
+
+    await using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+    {
+        sqlConnection.AccessTokenCallback = GetTokenCallback();
+        var sqlCommand = new SqlCommand(queryString, sqlConnection);
+        sqlCommand.CommandTimeout = 300;
+        sqlConnection.Open();
+        using (var reader = sqlCommand.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                retVal = reader[0] != DBNull.Value ? reader[0]?.ToString() ?? "0" : "0";
+                Console.WriteLine($"Total Row Count: {retVal}");
+            }
+        }
+   }
+    }
+    catch (SqlException ex)
+    {
+    Console.WriteLine($"Error in ExecuteReader running query {queryString} - Exception: {ex} ");
+    throw;
+    }
+    catch (Exception ex)
+    {
+    Console.WriteLine($"Error in ExecuteReader running query {queryString} - Exception: {ex} ");
+    throw;
+    }
+
+//found this code on the internet here https://stackoverflow.com/questions/78325276/package-azure-identity-v1-11-0-and-above-caused-an-error-with-sqlclient-and-dyna
+Func<SqlAuthenticationParameters, CancellationToken, Task<SqlAuthenticationToken?>> GetTokenCallback()
+{
+    var tenantId = configuration["AppSettings:TenantId"];
+    var clientId = configuration["AppSettings:ClientId"];
+    var clientSecret = configuration["AppSettings:ClientSecret"];
+    const string defaultScopeSuffix = ".default";
+
+    var cred = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+    return TokenCallback;
+
+    async Task<SqlAuthenticationToken?> TokenCallback(SqlAuthenticationParameters ctx, CancellationToken cancellationToken)
+    {
+        var validToken = _sqlAuthenticationToken != null && _sqlAuthenticationToken.ExpiresOn.UtcDateTime > DateTime.Now;
+        if (validToken) return _sqlAuthenticationToken;
+
+        string scope = ctx.Resource.EndsWith(defaultScopeSuffix)
+            ? ctx.Resource
+            : ctx.Resource + defaultScopeSuffix;
+
+        var token = await cred.GetTokenAsync(new TokenRequestContext([scope]), CancellationToken.None);
+        _sqlAuthenticationToken = new SqlAuthenticationToken(token.Token, token.ExpiresOn);
+        return _sqlAuthenticationToken;
+    }
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+﻿# Dataverse TDS Enpoint Connection Example
+
+## Introduction
+This is a Basic Proof Of Concept that demonstrates one possible way to connect and query a Dataverse TDS endpoint using .Net Standard. As it's a simple POC error handling and coding best practices are not fully implemented.
+
+## Supportablity Disclaimer 
+Currently the [Dataverse TDS Documentation](https://learn.microsoft.com/en-us/power-apps/developer/data-platform/dataverse-sql-query) indicates TDS access via code is in preview. I discourage using this in production until it is out of preview.
+
+## Prerequisites
+Confirm you can query using SMSS per the [Dataverse TDS Documentation](https://learn.microsoft.com/en-us/power-apps/developer/data-platform/dataverse-sql-query) when using the authentication method of 'Microsoft Entra Service Principal' 
+
+## Security
+I do not recommend hard coding your credentials in your code or config file. Use a secure method to store and retrieve your credentials like Azure Key Vault and Windows Credential Manager.
+
+## Getting Started
+Set the following values in the appsettings.json file:
+```json
+﻿{
+  "AppSettings": {
+    "SQLConnectionString": "Server=DATAVERSE URL.REGION.dynamics.com,5558;Database=DB FROM SSMS;Encrypt=True",
+    "TenantId": "",
+    "ClientId": "",
+    "ClientSecret": ""
+  }
+}
+```
+
+The SQL query is hard coded in the Program.cs. The code is written to return the first row of the query. If you  modify the query you might need to modfiy the code that reads the results. 
+```csharp
+string queryString = "select[Total Row Count] = count(*) from account";
+```
+
+## Disclaimer 
+``` text
+# DISCLAIMER
+# This software (or sample code) is not supported under any Microsoft standard
+# support program or service. The software is provided AS IS without warranty
+# of any kind. Microsoft further disclaims all implied warranties including,
+# without limitation, any implied warranties of merchantability or of fitness
+# for a particular purpose. The entire risk arising out of the use or
+# performance of the software and documentation remains with you. In no event
+# shall Microsoft, its authors, or anyone else involved in the creation,
+# production, or delivery of the software be liable for any damages whatsoever
+# (including, without limitation, damages for loss of business profits, business
+# interruption, loss of business information, or other pecuniary loss) arising
+# out of the use of or inability to use the software or documentation, even if
+# Microsoft has been advised of the possibility of such damages.
